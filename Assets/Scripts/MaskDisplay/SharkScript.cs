d339d708be163d114cc73c5bd09b8c46e88d7a04
umT14 <umutulastzk@gmail.com>
2026-02-01 14:32:36 +0300

    sfsggsfsefs

--- Assets/Scripts/MaskDisplay/SharkScript.cs
+++ Assets/Scripts/MaskDisplay/SharkScript.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SharkScript : MonoBehaviour
@@ -11,21 +12,29 @@ public class SharkScript : MonoBehaviour
 
     private float random;
     private float range => (facing == Facing.Left) ? -random : random;
+    private float posx => gameObject.transform.position.x + range;
     [SerializeField] private float velocity;
 
     private Facing facing;
 
     void Start()
     {
-        random = Random.Range(8, 4);
+        random = Random.Range(4, 8);
         facing = Facing.Left;
     }
     void Update()
     {
         gameObject.GetComponent<Rigidbody2D>().linearVelocityX = (facing == Facing.Left) ? -velocity : velocity;
-        if (gameObject.transform.position.x < range)
+        if (facing == Facing.Left && gameObject.transform.position.x < range)
         {
-            random = Random.Range(8, 4);
+            random = Random.Range(4, 8);
+            facing = (facing == Facing.Left) ? Facing.Right : Facing.Left;
+            //bool b = gameObject.GetComponentInChildren<SpriteRenderer>().flipX;
+            gameObject.GetComponentInChildren<SpriteRenderer>().flipX = !gameObject.GetComponentInChildren<SpriteRenderer>().flipX;
+        }
+        else if (facing == Facing.Right && gameObject.transform.position.x > range)
+        {
+            random = Random.Range(4, 8);
             facing = (facing == Facing.Left) ? Facing.Right : Facing.Left;
             //bool b = gameObject.GetComponentInChildren<SpriteRenderer>().flipX;
             gameObject.GetComponentInChildren<SpriteRenderer>().flipX = !gameObject.GetComponentInChildren<SpriteRenderer>().flipX;
